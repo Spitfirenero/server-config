15327dd9cf4edfa7ded8404f12dd03af3b3036ce
Chris Moser <u7923283@anu.edu.au>
2026-06-30 22:30:14 +1000

    updated the custom commands

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -3,10 +3,19 @@
 {
   environment.systemPackages = with pkgs; [
     (writeScriptBin "build" ''
-      #!/bin/sh
+      #!/usr/bin/env bash
+      set -euo pipefail
       cd /etc/nixos
       sudo git pull
       sudo nixos-rebuild switch --flake /etc/nixos#nixos --option warn-dirty false
+    '')
+    (writeScriptBin "clean" ''
+      #!/usr/bin/env bash
+      set -euo pipefail
+      sudo nix-env --delete-generations +5 --profile /nix/var/nix/profiles/system
+      sudo /run/current-system/bin/switch-to-configuration boot
+      sudo nix-collect-garbage -d
+      sudo nix-store --optimise
     '')
   ];
 }
