8948bc8c61f40ae9f6bfd0dc76a02b7f8c94a46e
Chris Moser <u7923283@anu.edu.au>
2026-07-01 01:59:14 +1000

    fixing samba issues

--- modules/firewall.nix
+++ modules/firewall.nix
@@ -4,6 +4,7 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ config.my.ssh.port 445 139 ];
+    allowedUDPPorts = [ 137 138 ];
     trustedInterfaces = [ "tailscale0" ];
   };
 }
--- modules/samba.nix
+++ modules/samba.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
 {
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
   services.samba = {
     enable = true;
     settings = {
@@ -10,11 +21,15 @@
         "netbios name" = "server";
         "security" = "user";
         "guest account" = "nobody";
+        "map to guest" = "Never";
+        "mdns name" = "mdns";
       };
       data = {
         "comment" = "SMB Data Share";
         "path" = "/srv/smb-share";
         "browseable" = "yes";
+        "guest ok" = "no";
+        "public" = "no";
         "read only" = "no";
         "create mask" = "0755";
         "directory mask" = "0755";
@@ -24,6 +39,23 @@
       };
     };
   };
+
+  environment.etc."avahi/services/smb.service".text = ''
+    <?xml version="1.0" standalone='no'?>
+    <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+    <service-group>
+      <name replace-wildcards="yes">%h</name>
+      <service>
+        <type>_smb._tcp</type>
+        <port>445</port>
+      </service>
+      <service>
+        <type>_device-info._tcp</type>
+        <port>0</port>
+        <txt-record>model=TimeCapsule8,119</txt-record>
+      </service>
+    </service-group>
+  '';
 
   systemd.tmpfiles.rules = [
     "d /srv/smb-share 0755 chris users - -"
